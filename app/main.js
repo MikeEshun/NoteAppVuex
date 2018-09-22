9b3401f7479fc59c526f31785d54002f061db315
myk_eshun <dev.mike.eshun@gmail.com>
2018-09-22 06:01:58 +0000

    state, mutations, actions, getters, store

--- app/main.js
+++ app/main.js
@@ -1,9 +1,47 @@
-const inputComponent = {
-  template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
+const state = {
+  notes: [],
+  timestamps: []
+};
+
+const mutations = {
+  ADD_NOTES( state, payload ) {
+    let newNote = payload;
+    state.notes.push(newNote);
+  },
+  ADD_TIMESTAMP( state, payload ) {
+    let newTimeStamp = payload;
+    state.notes.push(newTimeStamp);
+  },
+};
+
+const actions = {
+  addNote( context, payload ) {
+    context.commit( 'ADD_NOTE', payload );
+  },
+  addTimeStamp( context, payload ){
+    context.commit( 'ADD_TIMETSAMP', payload );
+  },
 }
+
+const getters = {
+  getNotes: state => state.notes,
+  getTimeStamps: state => state.timestamps,
+  getNoteCount: state => state.notes.length,
+};
+
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+});
+
+
+
 
 new Vue({
   el: '#app',
+  store,
   components: {
     'input-component': inputComponent
   }
